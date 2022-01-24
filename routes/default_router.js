1dd5fb1a6fbff5c34751b3fc4ecbc014c99d29fc
aungmyatmoe2008 <aungmyatmoe2008@gmail.com>
2022-01-24 20:43:34 +0800

    update setting

--- routes/default_router.js
+++ routes/default_router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res) => {
-    return res.status(200).send("<h1>Welcome from Ackstudio who is owend by Acktec</h1>")
+    return res.status(200).send("<h1>Welcome from Myanmar Culture Information</h1>")
 })
 
 module.exports = router;
